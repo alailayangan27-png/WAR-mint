8d06e2ebe520a74f9ba83d34c340992616194556
alailayangan27-png <alailayangan27@gmail.com>
2026-03-23 00:19:56 +0700

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,9 +1,7 @@
 export default async function handler(req, res){
-
   const r = await fetch(
     "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd"
   );
-
   const data = await r.json();
 
   res.json({ price: data.solana.usd });
