39dd7e55f9e3e3df2c372f56bbfe5a14cf5fa7bc
alailayangan27-png <alailayangan27@gmail.com>
2026-03-23 10:49:52 +0700

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,7 +1,20 @@
-let total = 0;
+import { supabase } from "../lib/db.js";
 
-export default function handler(req, res){
+export default async function handler(req, res){
+
+  const { data } = await supabase
+    .from("mints")
+    .select("total_sol");
+
+  let total = 0;
+
+  for(const row of data || []){
+    total += row.total_sol;
+  }
+
+  const war = total * 100000;
+
   res.json({
-    totalWar: total
+    total: war
   });
 }
